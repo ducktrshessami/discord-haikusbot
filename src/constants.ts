2beffb4e9e6dc18c00ed78040eac1b0f8d97415d
ducktrshessami <jwee761@gmail.com>
2023-04-04 16:10:00 -0700

    init db

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ import dotenv from "./dotenv.js";
 await dotenv();
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
+export const DB_FORCE = !!process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false";
