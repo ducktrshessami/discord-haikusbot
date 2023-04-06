02eafb65b07183d5eafee2e91755216d6ec79d2d
ducktrshessami <jwee761@gmail.com>
2023-04-05 20:41:04 -0700

    ignoreme command callback

--- src/discord/commands/ignoreme.ts
+++ src/discord/commands/ignoreme.ts
@@ -1,5 +1,14 @@
-import { SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { IgnoreUser } from "../../models/index.js";
 
 export const data = new SlashCommandBuilder()
     .setName("ignoreme")
     .setDescription("Opt out of replies");
+
+export async function callback(interaction: ChatInputCommandInteraction): Promise<void> {
+    await interaction.deferReply();
+    const [_, created] = await IgnoreUser.findOrCreate({
+        where: { id: interaction.user.id }
+    });
+    await interaction.editReply(created ? "" : ""); // TODO
+}
