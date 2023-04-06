bdc04fb10670b513def6bf75cc2aef458846986b
ducktrshessami <jwee761@gmail.com>
2023-04-05 19:32:49 -0700

    cache limiting

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,14 +1,44 @@
 import {
     Client,
     GatewayIntentBits,
-    PresenceData
+    GuildMember,
+    Options,
+    PresenceData,
+    User
 } from "discord.js";
 import activities from "./activities.js";
-import { DISCORD_PRESENCE_INTERVAL } from "../constants.js";
+import {
+    DISCORD_LIMITED_CACHE_MAX,
+    DISCORD_MESSAGE_LIFETIME,
+    DISCORD_PRESENCE_INTERVAL,
+    DISCORD_SWEEPER_INTERVAL,
+    DISCORD_THREAD_LIFETIME
+} from "../constants.js";
 
 const client = new Client({
     intents: GatewayIntentBits.Guilds,
-    presence: getPresence()
+    presence: getPresence(),
+    sweepers: {
+        threads: {
+            interval: DISCORD_SWEEPER_INTERVAL,
+            lifetime: DISCORD_THREAD_LIFETIME
+        },
+        messages: {
+            interval: DISCORD_SWEEPER_INTERVAL,
+            lifetime: DISCORD_MESSAGE_LIFETIME
+        }
+    },
+    makeCache: Options.cacheWithLimits({
+        MessageManager: DISCORD_LIMITED_CACHE_MAX,
+        UserManager: {
+            maxSize: DISCORD_LIMITED_CACHE_MAX,
+            keepOverLimit: keepClientUser
+        },
+        GuildMemberManager: {
+            maxSize: DISCORD_LIMITED_CACHE_MAX,
+            keepOverLimit: keepClientUser
+        }
+    })
 })
     .on("debug", console.debug)
     .on("warn", console.warn)
@@ -21,6 +51,10 @@ const client = new Client({
 
 function getPresence(): PresenceData {
     return { activities: [activities[Math.floor(Math.random() * activities.length)]] };
+}
+
+function keepClientUser(userOrMember: User | GuildMember): boolean {
+    return userOrMember.id === process.env.DISCORD_CLIENT_ID;
 }
 
 export async function login(): Promise<void> {
