fb3adef461dac331d8e5c513daa9f87efc4b3265
ducktrshessami <jwee761@gmail.com>
2023-04-07 14:38:08 -0700

    unignoreme command callback

--- src/discord/commands/unignoreme.ts
+++ src/discord/commands/unignoreme.ts
@@ -1,5 +1,14 @@
-import { SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { IgnoreUser } from "../../models/index.js";
 
 export const data = new SlashCommandBuilder()
     .setName("unignoreme")
     .setDescription("Opt back in to replies");
+
+export async function callback(interaction: ChatInputCommandInteraction): Promise<void> {
+    await interaction.deferReply();
+    const deleted = !!await IgnoreUser.destroy({
+        where: { id: interaction.user.id }
+    });
+    await interaction.editReply(deleted ? "" : ""); // TODO
+}
