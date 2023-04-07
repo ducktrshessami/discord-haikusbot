7fa1db1a0420dc8272ad370ab11b023e2356dfd9
ducktrshessami <jwee761@gmail.com>
2023-04-06 23:19:29 -0700

    activities

--- src/discord/activities.ts
+++ src/discord/activities.ts
@@ -1,4 +1,21 @@
-import { ActivitiesOptions } from "discord.js";
+import { ActivitiesOptions, ActivityType } from "discord.js";
 
-const activities: Array<ActivitiesOptions> = []; // TODO
+const activities: Array<ActivitiesOptions> = [
+    {
+        type: ActivityType.Playing,
+        name: "on words"
+    },
+    {
+        type: ActivityType.Listening,
+        name: "the 17 phonetic units"
+    },
+    {
+        type: ActivityType.Playing,
+        name: "with your heartstrings"
+    },
+    {
+        type: ActivityType.Watching,
+        name: "furuike ya"
+    }
+];
 export default activities;
