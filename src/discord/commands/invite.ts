25cd4289acfc64692048c6f17da51074075e0a1d
ducktrshessami <jwee761@gmail.com>
2023-04-09 14:48:47 -0700

    invite command callback

--- src/discord/commands/invite.ts
+++ src/discord/commands/invite.ts
@@ -1,5 +1,26 @@
-import { SlashCommandBuilder } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ChatInputCommandInteraction,
+    OAuth2Scopes,
+    PermissionFlagsBits,
+    SlashCommandBuilder
+} from "discord.js";
 
 export const data = new SlashCommandBuilder()
     .setName("invite")
     .setDescription("Send my invite link");
+
+export async function callback(interaction: ChatInputCommandInteraction): Promise<void> {
+    const button = new ButtonBuilder()
+        .setStyle(ButtonStyle.Link)
+        .setLabel("Invite")
+        .setURL(interaction.client.generateInvite({
+            scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands],
+            permissions: PermissionFlagsBits.ViewChannel | PermissionFlagsBits.SendMessages
+        }));
+    const row = new ActionRowBuilder<ButtonBuilder>()
+        .addComponents(button);
+    await interaction.reply({ components: [row] });
+}
