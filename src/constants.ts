8104501b618c7a939e357e159b617a179fac9dd2
ducktrshessami <cal.tf2.3@gmail.com>
2025-08-07 22:31:19 -0700

    refactor: native env loading

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
-import dotenv from "./dotenv.js";
-
-await dotenv();
-
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const DB_FORCE = !!process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false";
 export const DISCORD_PRESENCE_INTERVAL = process.env.DISCORD_PRESENCE_INTERVAL ? parseInt(process.env.DISCORD_PRESENCE_INTERVAL) : 1800000;
--- src/dotenv.ts
+++
@@ -1,9 +0,0 @@
-export default async function dotenv(): Promise<void> {
-    try {
-        const { config } = await import("dotenv");
-        config();
-    }
-    catch {
-        console.warn("Not using dotenv. Ensure environment variables are set");
-    }
-}
